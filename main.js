246722fe1008a3fc9330dcfe13d6b4f7ac7b7d67
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-02-27 22:59:04 +0545

    Created and styled HTML structure for Navigation, Main, Home, About, Skills section. JS for card animation, page scrollbar and section page animations. Problem with smooth scroll links in JS.

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-import './style.css'
+import 'vanilla-tilt';
+import SmoothScrollbar from 'smooth-scrollbar';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+//* Card Animation
+VanillaTilt.init(document.querySelectorAll('.skills__card'), {
+  max: 25,
+  speed: 400,
+  scale: 1.1,
+  glare: true,
+  'max-glare': 0.8,
+});
+
+//* Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'auto' });
+  });
+});
+
+//* Momentum Page Scrollbar
+SmoothScrollbar.init(document.querySelector('main'));
+
+//* Section Page Animations
+const sectionContainer = document.querySelectorAll('.section__container');
+const sectionObserver = new IntersectionObserver(
+  (entries, observer) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section__hidden');
+    observer.unobserve(entry.target);
+  },
+  {
+    root: null,
+    threshold: 0.17,
+  }
+);
+sectionContainer.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section__hidden');
+});
