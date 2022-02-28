21b4fa7220b12301e3348d60c8c787ddddb1928c
Prabin Shrestha <shresthaprabin315@gmail.com>
2022-02-28 14:38:01 +0545

    Added HTML and styled it for works and contact section, added intersection observer, resize observer in js, removed smooth scroll library.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import 'vanilla-tilt';
-import SmoothScrollbar from 'smooth-scrollbar';
 
 //* Card Animation
 VanillaTilt.init(document.querySelectorAll('.skills__card'), {
@@ -10,20 +9,17 @@ VanillaTilt.init(document.querySelectorAll('.skills__card'), {
   'max-glare': 0.8,
 });
 
-//* Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document
-      .querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'auto' });
-  });
+VanillaTilt.init(document.querySelectorAll('.works__card'), {
+  max: 25,
+  speed: 400,
+  scale: 1.1,
+  glare: true,
+  'max-glare': 0.5,
 });
 
-//* Momentum Page Scrollbar
-SmoothScrollbar.init(document.querySelector('main'));
+//* Smooth Scroll
 
-//* Section Page Animations
+//* Observing current position of window and applying section animations
 const sectionContainer = document.querySelectorAll('.section__container');
 const sectionObserver = new IntersectionObserver(
   (entries, observer) => {
@@ -41,3 +37,43 @@ sectionContainer.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section__hidden');
 });
+
+const contactContainer = document.querySelector('.section__contact-container');
+const contactObserver = new IntersectionObserver(
+  (entries, observer) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section__hidden');
+    document
+      .querySelector('.contact__form')
+      .classList.add('contact__button-show-line');
+    document
+      .querySelector('.contact__social-links')
+      .classList.add('contact__show-socials');
+    document
+      .querySelector('.contact__social-line')
+      .classList.add('contact__show-social-line');
+    observer.unobserve(entry.target);
+  },
+  {
+    root: null,
+    threshold: 0.17,
+  }
+);
+contactObserver.observe(contactContainer);
+contactContainer.classList.add('section__hidden');
+
+//* Observing the size of the body element and performing some operations
+const bodyObserver = new ResizeObserver(entries => {
+  const aboutTitle = document.querySelector('.about__title');
+  const navItems = document.querySelectorAll('.nav__item .bx');
+  const bodyObj = entries[0];
+
+  if (bodyObj.contentRect.width >= 468) {
+    aboutTitle.innerHTML = 'A LITTLE BIT ABOUT <div class="about__me">Me</div>';
+  } else if (bodyObj.contentRect.width < 468) {
+    aboutTitle.innerHTML = 'ABOUT <div class="about__me">Me</div>';
+  }
+});
+
+bodyObserver.observe(document.body);
